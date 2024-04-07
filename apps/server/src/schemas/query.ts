e2dead7ea6699c3d3cc534e6920f14b2e4adf596
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-04-07 14:09:30 -0300

    refactor: move queries to main query file

--- apps/server/src/schemas/query.ts
+++ apps/server/src/schemas/query.ts
@@ -1,12 +1,19 @@
 import { Node, Nodes } from "@/modules/node/register";
-import { Users } from "@/modules/user/queries/Users";
-import { GraphQLObjectType } from "graphql";
+import { UserLoader } from "@/modules/user/UserLoader";
+import { UserConnection } from "@/modules/user/UserType";
+import { GraphQLNonNull, GraphQLObjectType } from "graphql";
+import { connectionArgs } from "graphql-relay";
 
 export const query = new GraphQLObjectType({
 	name: "query",
 	description: "Root query",
 	fields: () => ({
-		users: Users,
+		users: {
+			type: new GraphQLNonNull(UserConnection.connectionType),
+			args: connectionArgs,
+			resolve: async (_, args, context) =>
+				await UserLoader.loadAll(context, args),
+		},
 		node: Node,
 		nodes: Nodes,
 	}),
