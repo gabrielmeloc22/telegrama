ccbfa685a083f68e49d0e4d8f3cb9f1a32f545db
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-05-26 21:31:06 -0300

    chore: adjust schema order

--- graphql.config.ts
+++ graphql.config.ts
@@ -2,8 +2,8 @@ import type { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
   schema: [
+    "./apps/web/node_modules/relay-compiler/relay-extensions.graphql",
     "http://localhost:4000/graphql",
-    // "./apps/web/node_modules/relay-compiler/relay-extensions.graphql",
   ],
   documents: ["apps/web/src/**/*.ts", "apps/web/src/**/*.tsx"],
   // include: "app/web/src/**/*.{tsx,ts}",
