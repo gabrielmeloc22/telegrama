5c4e75ac1a63ec4a39ab734460adba579dbb5e86
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-05-31 02:29:52 -0300

    feat: add subscription fn

--- apps/web/src/relay/environment.ts
+++ apps/web/src/relay/environment.ts
@@ -1,5 +1,6 @@
 "use client";
 
+import { type Sink, createClient } from "graphql-sse";
 import cookies, { parseCookies } from "nookies";
 import {
 	Environment,
@@ -8,11 +9,11 @@ import {
 	Observable,
 	RecordSource,
 	Store,
+	type SubscribeFunction,
 } from "relay-runtime";
 
 const fetchFn: FetchFunction = (params, variables) => {
 	const token = cookies.get().token ?? parseCookies(null);
-
 	const response = fetch(process.env.NEXT_PUBLIC_API_URL, {
 		method: "POST",
 		headers: [
@@ -28,8 +29,35 @@ const fetchFn: FetchFunction = (params, variables) => {
 	return Observable.from(response.then((data) => data.json()));
 };
 
+const subscriptionClient = createClient({
+	url: `${process.env.NEXT_PUBLIC_API_URL}stream`,
+	headers: () => {
+		const token = cookies.get().token ?? parseCookies(null);
+
+		return {
+			Authorization: `Bearer ${token}`,
+		};
+	},
+});
+
+const subscribeFn: SubscribeFunction = (operation, variables) => {
+	return Observable.create((sink) => {
+		if (!operation.text) {
+			return sink.error(new Error("Operation text cannot be empty"));
+		}
+		return subscriptionClient.subscribe(
+			{
+				operationName: operation.name,
+				query: operation.text,
+				variables,
+			},
+			sink as Sink,
+		);
+	});
+};
+
 export function createEnvironment() {
-	const network = Network.create(fetchFn);
+	const network = Network.create(fetchFn, subscribeFn);
 	const store = new Store(new RecordSource());
 
 	return new Environment({ store, network });
