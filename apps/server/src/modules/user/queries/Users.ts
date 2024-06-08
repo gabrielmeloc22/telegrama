6708166aff4caa895f6e3ac793a60b6ca2f37f6f
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-06-08 18:05:24 -0300

    feat: add query arg to users resolver

--- apps/server/src/modules/user/queries/Users.ts
+++ apps/server/src/modules/user/queries/Users.ts
@@ -1,15 +1,38 @@
 import type { Context } from "@/context";
-import { connectionArgs } from "@entria/graphql-mongo-helpers";
-import { type GraphQLFieldConfig, GraphQLNonNull } from "graphql";
+import {
+	FILTER_CONDITION_TYPE,
+	connectionArgs,
+	withFilter,
+} from "@entria/graphql-mongo-helpers";
+import escapeStringRegexp from "escape-string-regexp";
+import {
+	GraphQLNonNull,
+	GraphQLString,
+	type GraphQLFieldConfig,
+} from "graphql";
 import type { ConnectionArguments } from "graphql-relay";
 import { UserLoader } from "../UserLoader";
 import { UserConnection } from "../UserType";
 
-export const Users: GraphQLFieldConfig<unknown, Context, ConnectionArguments> =
-	{
-		type: new GraphQLNonNull(UserConnection.connectionType),
-		description: "All users",
-		args: connectionArgs,
-		resolve: async (_, args, context) =>
-			await UserLoader.loadAll(context, args),
-	};
+export const Users: GraphQLFieldConfig<
+	unknown,
+	Context,
+	ConnectionArguments & { query: string }
+> = {
+	type: new GraphQLNonNull(UserConnection.connectionType),
+	description: "All users",
+	args: {
+		...connectionArgs,
+		query: { type: GraphQLString },
+	},
+	resolve: async (_, args, context) =>
+		await UserLoader.loadAll(
+			context,
+			withFilter(args, {
+				username: {
+					type: FILTER_CONDITION_TYPE.MATCH_1_TO_1,
+					format: (v: string) => new RegExp(` /.*${escapeStringRegexp(v)}.*/`),
+				},
+			}),
+		),
+};
