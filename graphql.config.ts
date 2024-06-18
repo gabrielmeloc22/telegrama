1c7eb224f3f21be36e5fedd75b145cafe86ed9eb
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-06-18 20:39:52 -0300

    chore: adjust config and update deps

--- graphql.config.ts
+++ graphql.config.ts
@@ -1,12 +1,12 @@
 import type { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
-  schema: [
-    "./apps/web/node_modules/relay-compiler/relay-extensions.graphql",
-    "http://localhost:4000/graphql",
-  ],
-  documents: ["apps/web/src/**/*.ts", "apps/web/src/**/*.tsx"],
-  // include: "app/web/src/**/*.{tsx,ts}",
+	schema: [
+		"./apps/web/node_modules/relay-compiler/relay-extensions.graphql",
+		"./apps/server/schema/schema.graphql",
+	],
+	documents: ["apps/web/src/**/*.ts", "apps/web/src/**/*.tsx"],
+	// include: "app/web/src/**/*.{tsx,ts}",
 };
 
 export default config;
