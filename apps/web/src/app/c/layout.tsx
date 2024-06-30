a731484071dbda0ff78f3033d0a1de1d04887d15
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-06-30 02:00:43 -0300

    refactor: remove resizable panels

--- apps/web/src/app/c/layout.tsx
+++ apps/web/src/app/c/layout.tsx
@@ -1,23 +1,15 @@
 "use client";
 
 import { Sidebar } from "@/components/sidebar";
-import {
-	ResizableHandle,
-	ResizablePanel,
-	ResizablePanelGroup,
-} from "@ui/components";
 import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<div className="flex dark:bg-neutral-950/20">
-			<ResizablePanelGroup direction="horizontal">
-				<ResizablePanel maxSize={30} defaultSize={20} className="min-w-[300px]">
-					<Sidebar />
-				</ResizablePanel>
-				<ResizableHandle />
-				<ResizablePanel>{children}</ResizablePanel>
-			</ResizablePanelGroup>
+			<div className="w-1/2 max-w-[420px]">
+				<Sidebar />
+			</div>
+			<div className="w-full">{children}</div>
 		</div>
 	);
 }
